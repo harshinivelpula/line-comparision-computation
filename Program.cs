fec618e6d8a0c3d467d58382424b3116ad4f6c28
DELL <105187636+harshinivelpula@users.noreply.github.com>
2022-06-17 12:46:13 +0530

    uc1: check for length

--- Program.cs
+++ Program.cs
@@ -1,2 +1,12 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+
+Console.WriteLine("Enter x1");
+double x1 = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("Enter y1");
+double y1 = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("Enter x2");
+double x2 = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("Enter y2");
+double y2 = Convert.ToDouble(Console.ReadLine());
+double length = Math.Sqrt(Math.Pow((x2 - x1), 2) + Math.Pow((y2 - y1), 2));
+Console.WriteLine("length of this line is " + length);
